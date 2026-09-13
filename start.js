19066dddf13db870755e0d1f5326d527fd44d8dc
shoon94parkk-del <shoon94.parkk@gmail.com>
2026-09-14 00:57:35 +0900

    debug: inspect ScienceON internal search

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ const {scan}=require('./server4');
 
 const child=spawn(process.execPath,['server4.js'],{stdio:'inherit'});
 child.on('exit',code=>process.exit(code??1));
+spawn(process.execPath,['debug-scienceon.js'],{stdio:'inherit'});
 
 setTimeout(async()=>{
   const started=Date.now();
