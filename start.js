3a809addcc24c4d3d7471643de49865853c93f82
shoon94parkk-del <shoon94.parkk@gmail.com>
2026-09-14 07:52:46 +0900

    Run fixed web benchmark at startup

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const {scan}=require('./server4');
 
 const child=spawn(process.execPath,['server4.js'],{stdio:'inherit'});
 child.on('exit',code=>process.exit(code??1));
-spawn(process.execPath,['debug-filter.js'],{stdio:'inherit'});
+spawn(process.execPath,['debug-web-benchmark.js'],{stdio:'inherit'});
 
 setTimeout(async()=>{
   const started=Date.now();
